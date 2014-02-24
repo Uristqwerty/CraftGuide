daeeb1a0b6502b09cf4a49499e351091d8956bbc
Uristqwerty <Uristqwerty@gmail.com>
2014-02-24 00:12:50 -0500

    Large GregTech recipe update:  - Works with newer versions  - Layout can theoretically scale up to 9 inputs and 9 outputs  - Split Universal Macerator and Sturdy Grinder when output varies, to emphasize early-game progression routes  - Also logs to CraftGuide.log  - Extruder and Steam Hammer recipes

--- src/uristqwerty/CraftGuide/recipes/GregTechRecipes.java
+++ src/uristqwerty/CraftGuide/recipes/GregTechRecipes.java
@@ -11,6 +11,7 @@ import java.util.Map;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import uristqwerty.CraftGuide.CommonUtilities;
+import uristqwerty.CraftGuide.CraftGuideLog;
 import uristqwerty.CraftGuide.api.ChanceSlot;
 import uristqwerty.CraftGuide.api.CraftGuideAPIObject;
 import uristqwerty.CraftGuide.api.EUSlot;
@@ -145,38 +146,46 @@ public class GregTechRecipes extends CraftGuideAPIObject implements RecipeProvid
 					generator, new ItemStack(machine, 1, 113),
 					(ArrayList)recipeClass.getField("sCutterRecipes").get(null),
 					1, 1, -1, 0, false, null);
+			generateRecipes(
+					generator, new ItemStack(machine, 1, 115),
+					(ArrayList)recipeClass.getField("sExtruderRecipes").get(null),
+					2, 1, -1, 0, false, null);
+			generateRecipes(
+					generator, new ItemStack(machine, 1, 133),
+					(ArrayList)recipeClass.getField("sHammerRecipes").get(null),
+					1, 1, -1, 0, false, null);
 
 			generatePulverizerRecipes(generator,
-					Arrays.asList(new ItemStack(machine, 1, 64), new ItemStack(machine, 1, 130)),
+					new ItemStack(machine, 1, 64), new ItemStack(machine, 1, 130),
 					(Map<Integer, Object>)CommonUtilities.getPrivateValue(modHandlerClass, null, "sPulverizerRecipes"));
 		}
 		catch(ClassNotFoundException e)
 		{
-			e.printStackTrace();
+			CraftGuideLog.log(e, "Error while adding GregTech recipes:", true);
 		}
 		catch(IllegalArgumentException e)
 		{
-			e.printStackTrace();
+			CraftGuideLog.log(e, "Error while adding GregTech recipes:", true);
 		}
 		catch(SecurityException e)
 		{
-			e.printStackTrace();
+			CraftGuideLog.log(e, "Error while adding GregTech recipes:", true);
 		}
 		catch(IllegalAccessException e)
 		{
-			e.printStackTrace();
+			CraftGuideLog.log(e, "Error while adding GregTech recipes:", true);
 		}
 		catch(NoSuchFieldException e)
 		{
-			e.printStackTrace();
+			CraftGuideLog.log(e, "Error while adding GregTech recipes:", true);
 		}
 		catch(NoSuchMethodException e)
 		{
-			e.printStackTrace();
+			CraftGuideLog.log(e, "Error while adding GregTech recipes:", true);
 		}
 		catch(InvocationTargetException e)
 		{
-			e.printStackTrace();
+			CraftGuideLog.log(e, "Error while adding GregTech recipes:", true);
 		}
 	}
 
@@ -184,104 +193,104 @@ public class GregTechRecipes extends CraftGuideAPIObject implements RecipeProvid
 			int numInputs, int numOutputs, int constantEUt, int startEUOutputMult, boolean generated,
 			final String extraFormat) throws ClassNotFoundException, IllegalArgumentException, SecurityException, IllegalAccessException, NoSuchFieldException
 	{
-		Slot[] recipeSlots = new Slot[numInputs + numOutputs + 2];
-
-		if(numInputs == 1)
-		{
-			recipeSlots[0] = new ItemSlot(numOutputs <= 2? 12 : 3, 21, 16, 16, true).drawOwnBackground();
-		}
-		else if(numInputs == 2)
-		{
-			recipeSlots[0] = new ItemSlot(numOutputs <= 2? 12 : 3, 12, 16, 16, true).drawOwnBackground();
-			recipeSlots[1] = new ItemSlot(numOutputs <= 2? 12 : 3, 30, 16, 16, true).drawOwnBackground();
-		}
-		else
-		{
-			throw new IllegalArgumentException();
-		}
-
-		if(numOutputs == 1)
-		{
-			recipeSlots[numInputs + 0] = new ItemSlot(50, 21, 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT).drawOwnBackground();
-		}
-		else if(numOutputs == 2)
-		{
-			recipeSlots[numInputs + 0] = new ItemSlot(50, 12, 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT).drawOwnBackground();
-			recipeSlots[numInputs + 1] = new ItemSlot(50, 30, 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT).drawOwnBackground();
-		}
-		else
-		{
-			recipeSlots[numInputs + 0] = new ItemSlot(41, 12, 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT).drawOwnBackground();
-			recipeSlots[numInputs + 1] = new ItemSlot(59, 12, 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT).drawOwnBackground();
-
-			if(numOutputs == 3)
-			{
-				recipeSlots[numInputs + 2] = new ItemSlot(50, 30, 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT).drawOwnBackground();
-			}
-			else if(numOutputs == 4)
-			{
-				recipeSlots[numInputs + 2] = new ItemSlot(41, 30, 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT).drawOwnBackground();
-				recipeSlots[numInputs + 3] = new ItemSlot(59, 30, 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT).drawOwnBackground();
-			}
-			else
-			{
-				throw new IllegalArgumentException();
-			}
-		}
-
-		recipeSlots[numInputs + numOutputs + 0] = new ExtraSlot(numOutputs <= 2? 31 : 22, 30, 16, 16, machine).clickable().showName().setSlotType(SlotType.MACHINE_SLOT);
-
-		if(extraFormat == null)
-		{
-			recipeSlots[numInputs + numOutputs + 1] = new EUSlot(numOutputs <= 2? 31 : 22, 12);
-		}
-		else
-		{
-			recipeSlots[numInputs + numOutputs + 1] =
-				new EUSlot(numOutputs <= 2? 31 : 22, 12)
-				{
-					@Override
-					public List<String> getTooltip(int x, int y, Object[] data, int dataIndex)
-					{
-						List<String> lines = super.getTooltip(x, y, data, dataIndex);
-						lines.add(String.format(extraFormat, (((Object[])data[dataIndex])[2])));
-						return lines;
-					}
-				};
-		}
-
-		RecipeTemplate template = generator.createRecipeTemplate(recipeSlots, machine);
 
 		Class recipeClass = Class.forName("gregtechmod.api.util.GT_Recipe");
 		Field eutField = recipeClass.getField("mEUt");
 		Field durationField = recipeClass.getField("mDuration");
 		Field extraField = recipeClass.getField("mStartEU");
 
+		Field input1 = null;
+		Field input2 = null;
+		Field output1 = null;
+		Field output2 = null;
+		Field output3 = null;
+		Field output4 = null;
+
+		Field inputs = null;
+		Field outputs = null;
+
+		try
+		{
+			input1 = recipeClass.getField("mInput1");
+			input2 = recipeClass.getField("mInput2");
+			output1 = recipeClass.getField("mOutput1");
+			output2 = recipeClass.getField("mOutput2");
+			output3 = recipeClass.getField("mOutput3");
+			output4 = recipeClass.getField("mOutput4");
+		}
+		catch(NoSuchFieldException e)
+		{
+			inputs = recipeClass.getField("mInputs");
+			outputs = recipeClass.getField("mOutputs");
+		}
+
+		if(outputs != null)
+		{
+			for(Object recipe: recipes)
+			{
+				numInputs = Math.max(numInputs, recipeLength((ItemStack[])inputs.get(recipe)));
+				numOutputs = Math.max(numOutputs, recipeLength((ItemStack[])outputs.get(recipe)));
+			}
+		}
+		else if(numInputs < 1 || numInputs > 2 || numOutputs < 1 || numOutputs > 4)
+		{
+			throw new IllegalArgumentException();
+		}
+
+
+		Slot[] recipeSlots = layoutMachineSlots(machine, numInputs, numOutputs, extraFormat);
+		RecipeTemplate template = generator.createRecipeTemplate(recipeSlots, machine);
+
+		if(slotColumns(numInputs) + slotColumns(numOutputs) > 3)
+		{
+			template.setSize(79 + (slotColumns(numInputs) + slotColumns(numOutputs) - 3) * 18, 58);
+		}
+
+
 		for(Object recipe: recipes)
 		{
 			Object[] recipeContents = new Object[numInputs + numOutputs + 2];
-			recipeContents[0] = recipeClass.getField("mInput1").get(recipe);
 
-			if(numInputs == 2)
+			if(output4 != null)
 			{
-				recipeContents[1] = recipeClass.getField("mInput2").get(recipe);
+				recipeContents[0] = input1.get(recipe);
+
+				if(numInputs == 2)
+				{
+					recipeContents[1] = input2.get(recipe);
+				}
+
+				recipeContents[numInputs + 0] = output1.get(recipe);
+
+				if(numOutputs > 1)
+				{
+					recipeContents[numInputs + 1] = output2.get(recipe);
+				}
+
+				if(numOutputs > 2)
+				{
+					recipeContents[numInputs + 2] = output3.get(recipe);
+				}
+
+				if(numOutputs > 3)
+				{
+					recipeContents[numInputs + 3] = output4.get(recipe);
+				}
 			}
-
-			recipeContents[numInputs + 0] = recipeClass.getField("mOutput1").get(recipe);
-
-			if(numOutputs > 1)
+			else
 			{
-				recipeContents[numInputs + 1] = recipeClass.getField("mOutput2").get(recipe);
-			}
+				ItemStack[] inputStacks = (ItemStack[])inputs.get(recipe);
+				ItemStack[] outputStacks = (ItemStack[])outputs.get(recipe);
 
-			if(numOutputs > 2)
-			{
-				recipeContents[numInputs + 2] = recipeClass.getField("mOutput3").get(recipe);
-			}
+				for(int i = 0; i < Math.min(inputStacks.length, numInputs); i++)
+				{
+					recipeContents[i] = inputStacks[i];
+				}
 
-			if(numOutputs > 3)
-			{
-				recipeContents[numInputs + 3] = recipeClass.getField("mOutput4").get(recipe);
+				for(int i = 0; i < Math.min(outputStacks.length, numOutputs); i++)
+				{
+					recipeContents[i + numInputs] = outputStacks[i];
+				}
 			}
 
 			int eut = (constantEUt == -1)? eutField.getInt(recipe) : constantEUt;
@@ -306,8 +315,129 @@ public class GregTechRecipes extends CraftGuideAPIObject implements RecipeProvid
 		}
 	}
 
-	private void generatePulverizerRecipes(RecipeGenerator generator, List<ItemStack> machines, Map<Integer, Object> recipes) throws ClassNotFoundException, SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException
+	private int recipeLength(ItemStack[] itemStacks)
 	{
+		for(int i = itemStacks.length; i > 0; i--)
+		{
+			if(itemStacks[i - 1] != null)
+				return i;
+		}
+
+		return 0;
+	}
+
+	private Slot[] layoutMachineSlots(ItemStack machine, int numInputs, int numOutputs, final String extraFormat)
+	{
+		if(numInputs > 9 || numOutputs > 9)
+		{
+			throw new IllegalArgumentException("CraftGuide currently cannot handle GregTech machine recipes with more than 9 inputs or 9 outputs. In the unlikely case that more are needed, please report this to the current developer of CraftGuide.");
+		}
+
+		Slot[] recipeSlots = new Slot[numInputs + numOutputs + 2];
+
+		int recipeHOffset = Math.max(3, 30 - 9 * (slotColumns(numInputs) + slotColumns(numOutputs)));
+
+		for(int i = 0; i < numInputs; i++)
+		{
+			recipeSlots[i] = new ItemSlot(recipeHOffset + slotX(i, numInputs), 3 + slotY(i, numInputs), 16, 16, true).drawOwnBackground();
+		}
+
+		int outputHOffset = recipeHOffset + 20 + slotColumns(numInputs) * 18;
+
+		for(int i = 0; i < numOutputs; i++)
+		{
+			recipeSlots[numInputs + i] = new ItemSlot(outputHOffset + slotX(i, numOutputs), 3 + slotY(i, numOutputs), 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT).drawOwnBackground();
+		}
+
+		int centreHOffset = recipeHOffset + slotColumns(numInputs) * 18 + 1;
+
+		recipeSlots[numInputs + numOutputs + 0] = new ExtraSlot(centreHOffset, 30, 16, 16, machine).clickable().showName().setSlotType(SlotType.MACHINE_SLOT);
+
+		if(extraFormat == null)
+		{
+			recipeSlots[numInputs + numOutputs + 1] = new EUSlot(centreHOffset, 12);
+		}
+		else
+		{
+			recipeSlots[numInputs + numOutputs + 1] =
+				new EUSlot(centreHOffset, 12)
+				{
+					@Override
+					public List<String> getTooltip(int x, int y, Object[] data, int dataIndex)
+					{
+						List<String> lines = super.getTooltip(x, y, data, dataIndex);
+						lines.add(String.format(extraFormat, (((Object[])data[dataIndex])[2])));
+						return lines;
+					}
+				};
+		}
+
+		return recipeSlots;
+	}
+
+	private int slotX(int i, int numSlots)
+	{
+		switch(numSlots)
+		{
+		case 0: case 1: case 2:
+			return 0;
+
+		case 3: case 5:
+			return i == numSlots - 1? 9 : i % 2 == 0? 0 : 18;
+
+		case 4: case 6:
+			return i % 2 == 0? 0 : 18;
+
+		case 7:
+			return i < 2? (i == 0? 6 : 24) : i < 4? (18 * (i - 2)) : (i == 5? 6 : 24);
+
+		case 8:
+			return i < 6? (18 * ((i % 3) - 2)) : (i == 6? 6 : 24);
+
+		case 9:
+			return 18 * ((i % 3) - 2);
+
+		default:
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private int slotY(int i, int numSlots)
+	{
+		switch(numSlots)
+		{
+		case 0: case 1:
+			return 18;
+
+		case 2:
+			return i * 18 + 9;
+
+		case 3: case 4:
+			return (i / 2) * 18 + 9;
+
+		case 5: case 6:
+			return (i / 2) * 18;
+
+		case 7:
+			return i < 2? 0 : i < 5? 18 : 36;
+
+		case 8: case 9:
+			return (i / 3) * 18;
+
+		default:
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private int slotColumns(int numSlots)
+	{
+		return numSlots <= 0? 0 : numSlots <= 2? 1 : numSlots <= 6? 2 : 3;
+	}
+
+	private void generatePulverizerRecipes(RecipeGenerator generator, ItemStack electric, ItemStack steam, Map<Integer, Object> recipes) throws ClassNotFoundException, SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException
+	{
+		List<ItemStack> machines = Arrays.asList(electric, steam);
+
 		Slot[] recipeSlots = new Slot[] {
 				new ItemSlot(12, 21, 16, 16, true).drawOwnBackground(),
 				new ItemSlot(50, 12, 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT).drawOwnBackground(),
@@ -316,7 +446,24 @@ public class GregTechRecipes extends CraftGuideAPIObject implements RecipeProvid
 				new EUSlot(31, 12),
 		};
 
+		Slot[] recipeSlotsElectric = new Slot[] {
+				new ItemSlot(12, 21, 16, 16, true).drawOwnBackground(),
+				new ItemSlot(50, 12, 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT).drawOwnBackground(),
+				new ChanceSlot(50, 30, 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT).drawOwnBackground(),
+				new ExtraSlot(31, 30, 16, 16, electric).clickable().showName().setSlotType(SlotType.MACHINE_SLOT),
+				new EUSlot(31, 12),
+		};
+
+		Slot[] recipeSlotsSteam = new Slot[] {
+				new ItemSlot(12, 21, 16, 16, true).drawOwnBackground(),
+				new ItemSlot(50, 12, 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT).drawOwnBackground(),
+				new ChanceSlot(50, 30, 16, 16, true).setRatio(200).setSlotType(SlotType.OUTPUT_SLOT).drawOwnBackground(),
+				new ExtraSlot(31, 21, 16, 16, steam).clickable().showName().setSlotType(SlotType.MACHINE_SLOT),
+		};
+
 		RecipeTemplate template = generator.createRecipeTemplate(recipeSlots, machines.get(0));
+		RecipeTemplate templateElectric = generator.createRecipeTemplate(recipeSlotsElectric, machines.get(0));
+		RecipeTemplate templateSteam = generator.createRecipeTemplate(recipeSlotsSteam, machines.get(1));
 
 		Class recipeClass = Class.forName("gregtechmod.api.util.GT_PulverizerRecipe");
 		Method getInput = recipeClass.getMethod("getInput");
@@ -325,24 +472,97 @@ public class GregTechRecipes extends CraftGuideAPIObject implements RecipeProvid
 		Method getSecondaryOutputChance = recipeClass.getMethod("getSecondaryOutputChance");
 		Method getEnergy = recipeClass.getMethod("getEnergy");
 
+		Class prefixes = null;
+		Object ore = null;
+		Object denseOre = null;
+		Object endOre = null;
+		Object netherOre = null;
+		Method contains = null;
+		try
+		{
+			prefixes = Class.forName("gregtechmod.api.enums.OrePrefixes");
+			ore = prefixes.getField("ore").get(null);
+			denseOre = prefixes.getField("oreDense").get(null);
+			endOre = prefixes.getField("oreEnd").get(null);
+			netherOre = prefixes.getField("oreNether").get(null);
+			contains = prefixes.getMethod("contains", ItemStack.class);
+		}
+		catch(ClassNotFoundException e)
+		{
+		}
+		catch(NoSuchFieldException e)
+		{
+			prefixes = null;
+		}
+
+
 		for(Object recipe: recipes.values())
 		{
 			if(!recipeClass.isInstance(recipe))
 				continue;
 
-			Object[] recipeContents = new Object[] {
-					getInput.invoke(recipe),
-					getPrimaryOutput.invoke(recipe),
-					new Object[]{
-						getSecondaryOutput.invoke(recipe),
-						getSecondaryOutputChance.invoke(recipe),
-					},
-					machines,
-					new Object[]{getEnergy.invoke(recipe), 3},
-			};
+			Object input = getInput.invoke(recipe);
 
-			generator.addRecipe(template, recipeContents);
+			ItemStack output = (ItemStack)getPrimaryOutput.invoke(recipe);
+			if(prefixes != null && isOre(input, ore, denseOre, endOre, netherOre, contains))
+			{
+				Object[] recipeContentsElectric = new Object[] {
+						input,
+						output,
+						new Object[]{
+							getSecondaryOutput.invoke(recipe),
+							getSecondaryOutputChance.invoke(recipe),
+						},
+						electric,
+						new Object[]{getEnergy.invoke(recipe), 3},
+				};
+
+				generator.addRecipe(templateElectric, recipeContentsElectric);
+
+				ItemStack steamOutput = output;
+
+				if(steamOutput.stackSize > 1)
+				{
+					steamOutput = output.copy();
+					steamOutput.stackSize /= 2;
+				}
+
+				Object[] recipeContentsSteam = new Object[] {
+						input,
+						steamOutput,
+						new Object[]{
+							getSecondaryOutput.invoke(recipe),
+							getSecondaryOutputChance.invoke(recipe),
+						},
+						steam,
+				};
+
+				generator.addRecipe(templateSteam, recipeContentsSteam);
+			}
+			else
+			{
+				Object[] recipeContents = new Object[] {
+						input,
+						output,
+						new Object[]{
+							getSecondaryOutput.invoke(recipe),
+							getSecondaryOutputChance.invoke(recipe),
+						},
+						machines,
+						new Object[]{getEnergy.invoke(recipe), 3},
+				};
+
+				generator.addRecipe(template, recipeContents);
+			}
 		}
+	}
+
+	private boolean isOre(Object input, Object ore, Object denseOre, Object endOre, Object netherOre, Method contains) throws IllegalArgumentException, IllegalAccessException, InvocationTargetException
+	{
+		return	(Boolean)contains.invoke(ore, input) ||
+				(Boolean)contains.invoke(denseOre, input) ||
+				(Boolean)contains.invoke(endOre, input) ||
+				(Boolean)contains.invoke(netherOre, input);
 	}
 
 	@Override
